21557608d1180fae7190e8aa8440ce3eb32f7c91
guillaumemouchet <guillaume.mouchet01@outlook.com>
2023-04-05 21:52:53 +0200

    tested differents values for PT Detection

--- DetectionPT.py
+++ DetectionPT.py
@@ -13,15 +13,21 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 def what_text(imgResultGrey):
     allText = ""
     img_blur = cv.GaussianBlur(imgResultGrey, (3, 3), 0)
-    gray = cv.threshold(img_blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    gray = cv.threshold(img_blur, 150, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
     gray = cv.medianBlur(gray, 3)
-    gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (3,3)))
-    gray = cv.dilate(gray, (3,3), iterations=2)
-    gray = cv.erode(gray, (3,3), iterations=3)
-    gray = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (2,2)))
+    gray = cv.dilate(gray, (3,3), iterations=3)
+    gray = cv.erode(gray, (3,3), iterations=2)
+    gray = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
         # Get the data of the image with Tesseract
     results = pytesseract.image_to_string(gray, config="--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789/*X")
     
+    ## --psm 1 --oem 3 works for black
+    ## --psm 3 --oem 3 works for black
+
+    ## --psm 6 --oem 3 works for red and multiple
+
+    ## And erosion of 6 works for blue
     print(results)
     if results.strip() == '*/*': # * the value is changing
         power, toughness = 0, 0
@@ -35,10 +41,62 @@ def what_text(imgResultGrey):
 
 
     print(f'Power: {power}, Toughness: {toughness}')
-    #cv.imshow("im2", imgResultGrey)
+    cv.imshow("im2", gray)
     cv.waitKey(0)
     return allText
 
+def what_text2(imgResultGrey):
+    allText = ""
+    img_blur = cv.GaussianBlur(imgResultGrey, (3, 3), 0)
+    gray = cv.threshold(img_blur, 150, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    gray = cv.medianBlur(gray, 3)
+    gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (2,2)))
+    gray = cv.dilate(gray, (3,3), iterations=3)
+    gray = cv.erode(gray, (3,3), iterations=2)
+    gray = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+        # Get the data of the image with Tesseract
+    results = pytesseract.image_to_data(gray,output_type=Output.DICT, config="--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789/*X")
+    
+    for i in range(0, len(results["text"])):       
+        # Get text and confidance in the result
+        text = results["text"][i]
+        conf = int(results["conf"][i])
+        
+    #      if results.strip() == '*/*': # * the value is changing
+    #     power, toughness = 0, 0
+    # elif 'X' in results: # X is defined by the cost
+    #     power, toughness = 0, 0
+    # else:
+    #     try:
+    #         power, toughness = map(int, results.split('/'))
+    #     except:
+    #         power, toughness = "Error", "Error"
+
+
+    # print(f'Power: {power}, Toughness: {toughness}')
+        print(text)
+        print(conf)
+        # Filter out week confidance and empty text
+        if conf > 85:
+            allText += text+ " "
+            
+            # Show the confidence of each text
+            print("Confidence: {}".format(conf))
+            print("Text: {}".format(text))
+            print("")
+
+            text = "".join(text).strip()
+            # Write text on the image
+            x = results["left"][i]
+            y = results["top"][i]
+            cv.putText(imgResultGrey,
+                        text,
+                        (x, y - 10), 
+                        cv.FONT_HERSHEY_SIMPLEX,
+                        1.2, (0, 255, 255), 3)
+    cv.imshow("im2", imgResultGrey)
+    cv.waitKey(0)
+    return allText
 
 
 def test_all_cards():
@@ -57,6 +115,7 @@ def test_all_cards():
 
         # take only top right corner
         img_crop = imgResult[w-150:w, h-250:h]
+
         #convert to grey
         imgResultGrey = cv.cvtColor(img_crop,cv.COLOR_BGR2GRAY)
 
